9a4529dbbb0e73172edbc856890f40a7d56024e7
Ali Suleiman <alisuleiman@Alis-MacBook-Pro.local>
2021-02-02 16:27:41 -0500

    ..

--- netflixapi/setup.sh
+++ netflixapi/setup.sh
@@ -1,5 +1,5 @@
-virtualenv --python python3 ~/envs/netflixv
-source ~/envs/netflixv/bin/activate
-pip install -r requirements.txt
+#virtualenv --python python3 ~/envs/netflixv
+#source ~/envs/netflixv/bin/activate
+#pip install -r requirements.txt
 python orms.py
 gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker
