08b948ce13c721d6fde3ae35a801b009e42a2102
Ali Suleiman <alisuleiman@Alis-MacBook-Pro.local>
2021-02-02 16:23:11 -0500

    add more setup for deploy

--- netflixapi/setup.sh
+++ netflixapi/setup.sh
@@ -1,3 +1,5 @@
-pip install fastapi
-pip install uvicorn
+virtualenv --python python3 ~/envs/netflixv
+source ~/envs/netflixv/bin/activate
+pip install -r requirements.txt
 python orms.py
+gunicorn main:app -w 4 -k uvicorn.workers.UvicornWorker
